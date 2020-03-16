aed26d891f3ac8e8a1df4474ef4ca1d4d9742aa5
Freddy Munive <munivef@yahoo.com>
2020-03-16 16:37:13 -0500

    changing a production mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCWTyQB4fnqMYFuibXH9RQgRWZZkmoLUu0",
+    authDomain: "crud-angular-584de.firebaseapp.com",
+    databaseURL: "https://crud-angular-584de.firebaseio.com",
+    projectId: "crud-angular-584de",
+    storageBucket: "crud-angular-584de.appspot.com",
+    messagingSenderId: "169904621007"
+  }
 };
